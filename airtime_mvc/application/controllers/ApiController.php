7e6920cb9532c7f70adad9609a50be944a837faa
AlexG <alexandr.a.gusev@gmail.com>
2016-06-18 00:57:43 +0500

    WIP started implementing auto-dj trigger // fixes

--- airtime_mvc/application/controllers/ApiController.php
+++ airtime_mvc/application/controllers/ApiController.php
@@ -1121,6 +1121,8 @@ class ApiController extends Zend_Controller_Action
         Logging::info("Uh-oh, autoDJ just got triggered!");
 
         //todo: add actual autodj stuff
+        
+        $this->_helper->json->sendJson(array()); //needed to override zend framework rendering, silly, I know
     }
 
     public function getBootstrapInfoAction()
